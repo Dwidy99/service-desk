3bd67e899339523dcc6f34702c6c2356e5fa8fbf
Dwi <98dwiyulianto@gmail.com>
2026-04-08 14:10:49 +0700

    Service Desk : add some code in server.js in backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const path = require('path')
 const express = require('express')
 const cors = require('cors')
 require('colors')
 require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+
 const PORT = process.env.PORT || 5000
 
 connectDB()
@@ -14,30 +14,23 @@ const app = express()
 app.use(
   cors({
     origin: ['http://localhost:3000', 'https://service-desk-eta.vercel.app'],
-    credentials: true,
   })
 )
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 app.use('/api/members', require('./routes/memberRoutes'))
 app.use('/api/departments', require('./routes/departmentRoutes'))
 app.use('/api/profile', require('./routes/profileRoutes'))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-  app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-  })
-} else {
-  app.get('/', (_, res) => {
-    res.status(200).json({ message: 'Welcome to the Service Desk API' })
-  })
-}
+// Root route for backend health check
+app.get('/', (_, res) => {
+  res.status(200).json({ message: 'Welcome to the Service Desk API' })
+})
 
 app.use(errorHandler)
 
