43d4e560806b453a0f9f540ba0241c9cdf5e459c
Dwi <98dwiyulianto@gmail.com>
2026-04-08 14:04:29 +0700

    Service Desk : install cors and add some code in server.js in backend

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 require('colors')
 require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
-// Connect to database
 connectDB()
 
 const app = express()
 
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://service-desk-eta.vercel.app'],
+    credentials: true,
+  })
+)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 app.use('/api/members', require('./routes/memberRoutes'))
 app.use('/api/departments', require('./routes/departmentRoutes'))
 app.use('/api/profile', require('./routes/profileRoutes'))
 
-// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
   })
