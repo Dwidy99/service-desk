03bde6e06a229ea246a8d7b2df7afd3c839b312d
Dwi <98dwiyulianto@gmail.com>
2026-04-10 00:55:53 +0700

    FrontEnd - fix(auth): implement 24-hour token expiration

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -2,6 +2,16 @@ import axios from 'axios'
 
 const API_URL = `${process.env.REACT_APP_API_URL}/api/users/`
 
+const checkTokenExpiration = () => {
+  const user = JSON.parse(localStorage.getItem('user'))
+  if (user) {
+    const expirationTime = user?.expirationTime
+    const currentTime = new Date().getTime() / 1000 // Get current timestamp in seconds
+    return expirationTime > currentTime
+  }
+  return false
+}
+
 // Register user
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData)
@@ -13,7 +23,12 @@ const login = async (userData) => {
   const response = await axios.post(API_URL + 'login', userData)
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    const expirationTime = new Date().getTime() / 1000 + 86400 // Add 24 hours to current time (86400 seconds)
+    const userDataWithExpiration = {
+      ...response.data,
+      expirationTime: expirationTime,
+    }
+    localStorage.setItem('user', JSON.stringify(userDataWithExpiration))
   }
 
   return response.data
@@ -25,10 +40,18 @@ const logout = () => {
   localStorage.removeItem('token')
 }
 
+// If the token has expired, log the user out automatically
+const autoLogout = () => {
+  if (!checkTokenExpiration()) {
+    logout()
+  }
+}
+
 const authService = {
   register,
   logout,
   login,
+  autoLogout,
 }
 
 export default authService
